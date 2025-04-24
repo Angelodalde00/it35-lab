cebbd7627da998396c78773be4fd8f2372895d48
Angelodalde00 <lesterdalde27@gmail.com>
2025-04-24 15:55:56 +0800

    Updates

--- assets/ios.transition-legacy-BVRh3ivR.js
+++ assets/ios.transition-legacy-BVRh3ivR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BH_0wqZ6.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-hXtuzTvN.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
