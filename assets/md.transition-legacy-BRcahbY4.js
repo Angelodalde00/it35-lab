d807609dfee2aaf98543f44101fbdab566e9659d
Angelodalde00 <lesterdalde27@gmail.com>
2025-05-05 08:34:02 +0800

    Updates

--- assets/md.transition-legacy-BRcahbY4.js
+++ assets/md.transition-legacy-BRcahbY4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BsnwPdXG.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BHl5246q.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
